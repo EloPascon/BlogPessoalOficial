6a91ecf3659b646fd67021f139c43bb1a11ccacc
elo pascon <elopascon@gmail.com>
2022-05-31 16:51:21 -0300

    atualização CRUD tema

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -21,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
 
@@ -31,6 +32,9 @@ public class PostagemController {
 
 	@Autowired
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping
 	public ResponseEntity<List<Postagem>> getAll(){
@@ -52,15 +56,21 @@ public class PostagemController {
 	}
 	@PostMapping
 	public ResponseEntity <Postagem> postPostagem (@Valid @RequestBody Postagem postagem){
+		if (postagem.getTema().getId() == null || !temaRepository.existsById(postagem.getTema().getId())){
+			return ResponseEntity.badRequest().build();
+		}
 		
 		return ResponseEntity.status( HttpStatus.CREATED).body(postagemRepository.save(postagem));
 	}
 	@PutMapping
 	public ResponseEntity <Postagem> putPostagem (@Valid @RequestBody Postagem postagem){
 		
-		if (postagem.getId() == null)
-			return ResponseEntity.badRequest().build();
-		
+		if (postagem.getId() == null ||
+				postagem.getTema().getId() == null ||
+				!temaRepository.existsById(postagem.getTema().getId())){
+					return ResponseEntity.badRequest().build();
+				}
+	
 		return postagemRepository.findById(postagem.getId())
 				  .map(resposta ->ResponseEntity.status(HttpStatus.OK).body(postagemRepository.save(postagem)))
 				  .orElse(ResponseEntity.notFound().build());
--- src/main/java/com/generation/blogpessoal/model/Postagem.java
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -9,12 +9,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table (name= "tb_Postagens")
@@ -35,6 +38,10 @@ public class Postagem {
 	@UpdateTimestamp
   private LocalDateTime data;
 
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 public Long getId() {
 	return id;
 }
@@ -65,6 +72,11 @@ public LocalDateTime getData() {
 
 public void setData(LocalDateTime data) {
 	this.data = data;
+}
+
+public Postagem getTema() {
+	// TODO Auto-generated method stub
+	return null;
 }
   
   
